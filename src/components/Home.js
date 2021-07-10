a287c294bbf9ecc6fdd83b50c893ee82631773d5
Bik182 <bikal_t@yahoo.com>
2021-07-09 22:08:02 -0500

    gifs

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import CarouselProjects from "./CarouselProjects";
 import Slider from "react-slick";
 import myPicture from "../media/me.jpg";
 import NavBar from "./NavBar";
+import biksGifsLogo from "../media/logo-red.png";
 
 export default function Home() {
   const [width, setWindowWidth] = useState(0);
@@ -17,13 +18,15 @@ export default function Home() {
     const width = window.innerWidth;
     setWindowWidth(width);
   };
+  let headerMaxImageWidth = "10";
+  let logoMultipler = (1920 / width) * headerMaxImageWidth + "%";
 
   let fontSizeMultiplier = width / 1920;
-  let imageMultiplier = 1920/width;
+  let imageMultiplier = 1920 / width;
   let currTODOMaxFontSize = 50 * fontSizeMultiplier;
   let currDescriptionFontSize = 25 * fontSizeMultiplier;
-  let currHeight = (50 + imageMultiplier) + "%";
-  let currWidth = (25 + imageMultiplier) + "%";
+  let currHeight = 50 + imageMultiplier + "%";
+  let currWidth = 25 + imageMultiplier + "%";
   if (currTODOMaxFontSize < 20) {
     currTODOMaxFontSize = 20;
   }
@@ -35,6 +38,16 @@ export default function Home() {
       <NavBar screenWidth={width}></NavBar>
       <div className="homeHeader">
         <div>
+          <div className="gif-logo-container">
+            <a class="gif-link" target="_blank" href="https://biksgifs.com/">
+              <img
+                style={{ width: logoMultipler, height: logoMultipler }}
+                className="gif-logo"
+                alt="biks gifs link"
+                src={biksGifsLogo}
+              ></img>
+            </a>
+          </div>
           <CarouselProjects screenWidth={width}></CarouselProjects>
         </div>
         <div className="bottomHeader">
@@ -56,12 +69,11 @@ export default function Home() {
           >
             Email Me!
           </a>
-          <div className="image-div"   style={{ height: currHeight, width: currWidth }}>
-          <img
-            className="me"
-            src={myPicture}
-          
-          />
+          <div
+            className="image-div"
+            style={{ height: currHeight, width: currWidth }}
+          >
+            <img className="me" src={myPicture} />
           </div>
         </div>
       </div>
